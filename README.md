42b55a5151caa63ec45e1ec73c51930a9a805592
chenmingzhen <214164051@qq.com>
2020-05-17 10:33:54 +0800

    提交Vue-ebook后端API源码

---
+++ README.md
@@ -0,0 +1,28 @@
+# ebook后端API
+
+## 思路
+```
+1.先打开数据库，插入书籍的资料
+2.编写node.js 编写连接数据库接口 监听本地3000端口
+3.编写API app.get  '/book/list' '/book/home' 等
+  通过本地ip加端口加指定位置 获得json数据
+4.打开vue项目 将之前的mock模拟数据取消 
+  .env.development的所有API地址进行修改
+```
+
+## 环境安装
+```
+1.express API
+    cnpm i express -S
+2.跨域问题
+   cnpm i -S cors  
+
+```
+
+## 填坑
+```
+1.调用跨域是app.use(cors());
+ 不是app.use(cors)啊
+```
+     
+
---
+++ const.js
@@ -0,0 +1,51 @@
+const env = require('./env');
+let resUrl;
+let mp3FilePath;
+let dbHost;
+let dbUser;
+let dbPwd;
+
+if (env === 'dev') {
+    resUrl = 'http://192.168.1.106:8081';
+    // mp3FilePath = '/Users/sam/Desktop/resource/mp3';
+    dbHost = 'localhost';
+    dbUser = 'root';
+    dbPwd = 'cmz214164051';
+} else if (env === 'prod') {
+
+}
+
+const category = [
+    'Biomedicine',
+    'BusinessandManagement',
+    'ComputerScience',
+    'EarthSciences',
+    'Economics',
+    'Engineering',
+    'Education',
+    'Environment',
+    'Geography',
+    'History',
+    'Laws',
+    'LifeSciences',
+    'Literature',
+    'SocialSciences',
+    'MaterialsScience',
+    'Mathematics',
+    'MedicineAndPublicHealth',
+    'Philosophy',
+    'Physics',
+    'PoliticalScienceAndInternationalRelations',
+    'Psychology',
+    'Statistics'
+];
+
+module.exports = {
+    resUrl,
+    category,
+    mp3FilePath,
+    dbHost,
+    dbUser,
+    dbPwd
+};
+
